71cb0524f54b97f5962e031decdc342083beec92
toadkiller24 <daotoad676@gmail.com>
2024-11-01 16:45:35 +0200

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Main from "./components/Main";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div className=" bg-[#472d3c]">
       <Main />
-      <Footer />
     </div>
   );
 }
